340eb2d0f04ffa5f2dbbb108c949cec9a887ee80
Tom Hinton <tom.hinton@cse.org.uk>
2020-11-19 14:38:02 +0000

    Install files

---
+++ default.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {}
+, stdenv ? pkgs.stdenv
+, lib ? pkgs.lib
+, makeWrapper ? pkgs.makeWrapper
+, babashka ? pkgs.babashka
+, doctl ? pkgs.doctl
+, openssh ? pkgs.openssh
+, nix ? pkgs.nix
+} :
+stdenv.mkDerivation rec {
+  name = "oceanix";
+  src = ./.;
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -R $src/* $out/
+    makeWrapper $out/oceanix $out/bin/oceanix \
+      --prefix PATH : ${babashka}/bin \
+      --prefix PATH : ${doctl}/bin \
+      --prefix PATH : ${openssh}/bin \
+      --prefix PATH : ${nix}/bin
+  '';
+}
