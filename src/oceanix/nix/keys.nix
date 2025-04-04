1ea4852d664f16312d18eaf74f7a074395f74489
Tom Hinton <tom.hinton@cse.org.uk>
2025-04-04 10:24:41 +0100

    fix type

--- src/oceanix/nix/keys.nix
+++ src/oceanix/nix/keys.nix
@@ -24,7 +24,7 @@ with pkgs.lib;
 
       options.transient = mkOption {
         default = true;
-        type = types.boolean;
+        type = types.bool;
         description = "Wipe key on reboot if true; restore from a copy on the machine otherwise.";
       };
       
